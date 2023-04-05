73c4b5ce90414610cd4fbd57e2a46923c189521e
LutfiNR <lutfinurrohman5@gmail.com>
2023-04-06 04:13:55 +0700

    upik

--- app/src/main/java/com/hiev/app/ArActivity.java
+++ app/src/main/java/com/hiev/app/ArActivity.java
@@ -16,18 +16,12 @@ import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import java.util.Objects;
-
 public class ArActivity extends AppCompatActivity {
-    private ArFragment arCam; //object of ArFragment Class
-    private int clickNo = 0; //helps to render the 3d model only once when we tap the screen
+    private ArFragment arCam;
+    private int clickNo = 0;
     public static boolean checkSystemSupport(Activity activity) {
-
-        //checking whether the API version of the running Android >= 24 that means Android Nougat 7.0
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-
             String openGlVersion = ((ActivityManager) Objects.requireNonNull(activity.getSystemService(Context.ACTIVITY_SERVICE))).getDeviceConfigurationInfo().getGlEsVersion();
-
-            //checking whether the OpenGL version >= 3.0
             if (Double.parseDouble(openGlVersion) >= 3.0) {
                 return true;
             } else {
@@ -40,27 +34,19 @@ public class ArActivity extends AppCompatActivity {
             activity.finish();
             return false;
         }
-
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar);
-
         if (checkSystemSupport(this)) {
-
             arCam = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arCameraArea);
-            //ArFragment is linked up with its respective id used in the activity_main.xml
-
             arCam.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
                 clickNo++;
-                //the 3d model comes to the scene only when clickNo is one that means once
                 if (clickNo == 1) {
                     Anchor anchor = hitResult.createAnchor();
                     AnchorNode anchorNode = new AnchorNode(anchor);
                     anchorNode.setParent(arCam.getArSceneView().getScene());
-                    //render maps
                     ModelRenderable.builder()
                             .setSource(this, R.raw.anchor)
                             .setIsFilamentGltf(true)
@@ -71,8 +57,6 @@ public class ArActivity extends AppCompatActivity {
                                 builder.setMessage("Somthing is not right" + throwable.getMessage()).show();
                                 return null;
                             });
-
-
                     arCam.getArSceneView().getScene().addChild(anchorNode);
                     addModel(R.raw.bapak,anchorNode,0f,0f,0f,0.005f,"adolf");
                     addModel(R.raw.kebon,anchorNode,0f,0f,0f,0.005f,"pertanian");
@@ -83,21 +67,16 @@ public class ArActivity extends AppCompatActivity {
                     Toast.makeText(this, "Tekan Gambar Untuk Informasi Detil", Toast.LENGTH_LONG).show();
                 }
             });
-
         } else {
-
             return;
-
         }
     }
-
     private void createAnchor(AnchorNode anchorNode, ModelRenderable modelRenderable){
         Node modelMaps = new Node();
         modelMaps.setParent(anchorNode);
         modelMaps.setRenderable(modelRenderable);
         modelMaps.setLocalScale(new Vector3(0.001f, 0.001f, 0.001f));
     }
-
     private void addModel(Integer modelSource, AnchorNode anchorNode, float xPos, float yPos, float zPos, float scale,String id){
         ModelRenderable.builder()
                 .setSource(this, modelSource)
--- app/src/main/java/com/hiev/app/DetailActivity.java
+++ app/src/main/java/com/hiev/app/DetailActivity.java
@@ -2,27 +2,20 @@ package com.hiev.app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 public class DetailActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
         TextView tJudul = findViewById(R.id.judul);
         TextView tIsi = findViewById(R.id.isi);
         String[] judul = getResources().getStringArray(R.array.dataJudul);
         String[] isi = getResources().getStringArray(R.array.dataIsi);
         String[] list = {"adolf","sekolah","tokoh","persinggahan","pertanian"};
-
         Intent intent = getIntent();
         String id = intent.getStringExtra("passid");
-
         for (int i = 0; i < list.length; i++) {
             if (id.equals(list[i])) {
                 tJudul.setText(judul[i]);
@@ -30,10 +23,7 @@ public class DetailActivity extends AppCompatActivity {
                 break;
             }
         }
-
     }
-
-
     @Override
     public void onBackPressed() {
         super.onBackPressed();
